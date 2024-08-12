0650373b25f3934f32fcfdb3c3d1694988ebd23d
Joanzam <joanzam.com@gmail.com>
2024-08-12 18:23:57 +0000

    my first commit

--- src/app.py
+++ src/app.py
@@ -1,9 +1,27 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/todos', methods=['GET'])
-def hello_world():
-    return '<h1>Hello!</h1>'
+from flask import Flask, jsonify, request
 
+app = Flask(__name__)
+
+todos = [ 
+   { "label": "My first task", "done": False } ]
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.get_json()
+    print("Incoming request with the following body", request_body)
+    todos.append(request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position): 
+    if 0 <= position < len(todos):
+        deleted_item = todos.pop(position)
+    print("This is the position to delete:", position)
+    return jsonify(todos)
 # Estas dos líneas siempre deben estar al final de tu archivo app.py
 
 if __name__ == '__main__':
